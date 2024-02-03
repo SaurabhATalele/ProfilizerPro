effe75f373b86a58b54011b956bf49658a53ad53
jay <jayram.bagal@appyhigh.com>
2024-02-03 13:09:53 +0530

    added register

---
+++ client/app/register/page.jsx
@@ -0,0 +1,22 @@
+"use client";
+import Signup from "@/Components/Signup/Signup";
+import React from "react";
+import { useTheme } from "@/Context/ThemeContext";
+import Navbar from "@/Components/Navbar/Navbar";
+
+export default function page() {
+  const { darkMode } = useTheme();
+
+  return (
+    <div className={`${darkMode ? "dark" : ""}`}>
+      <main
+        className={` flex p-10 max-h-screen max-w-screen flex-col items-center justify-between bg-white text-black  dark:bg-black dark:text-white`}
+      >
+        <Navbar />
+        <Signup />
+      </main>
+    </div>
+  );
+}
+
+// export default page;
