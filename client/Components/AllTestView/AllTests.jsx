fab0b4903e67ca198c8288d83dc641fe7d20642e
Sneha <snehamane2532@gmail.com>
2024-03-01 23:48:05 +0530

    added all tests page

---
+++ client/Components/AllTestView/AllTests.jsx
@@ -0,0 +1,35 @@
+"use client"
+
+import React,{useState,useEffect} from 'react'
+import Card from './Card'
+import axios from 'axios'
+
+
+const AllTests = () => {
+    const [tests, setTests] = useState([]);
+
+  const getTests = async () => {
+    try {
+      const res = await axios.get("/api/v1/assignment");
+      setTests(res.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getTests();
+    console.log(tests);
+  }, []);
+  return (
+    <div className='w-3/4 py-24 flex flex-col gap-5'>
+
+        <h1 className='text-xl font-bold text-primary-light dark:text-primary-dark'>All tests</h1>
+        
+            <Card tests={tests}/>
+        
+
+    </div>
+  )
+}
+
+export default AllTests
--- client/Components/examDash/ExamDash.js
+++ client/Components/examDash/ExamDash.js
@@ -12,43 +12,43 @@ function ExamDash() {
         <div className=" flex flex-col  mt-28  text-center justify-between  w-1/3 p-5">
           <div className=" h-auto grid  grid-cols-6 gap-8 ">
             {ques.map((ele) => { 
-              return <Button num={ele} status="marked" />;
+              return <Button num={ele} status="marked"  />;
             })}
           </div>
           <div className="p-10">
             <button className=" w-28 h-10 bg-green-400 rounded-md ">Submit</button>
           </div>
         </div>
-        <div className="flex flex-col  mt-28  text-center justify-between w-2/3 border">
-            <div className=" flex gap-8 border">
+        <div className="flex flex-col  mt-28  text-center justify-between w-2/3 ">
+            <div className=" flex gap-8 ">
                 <div className="w-1/2 ">
-                    <h2>Question number 1</h2>
-                    <p>
-                        How many datatypes in python ?
+                    <h2 className=" text-xl text-slate-700 font-semibold">Question number: 1</h2>
+                    <p className="  text-xl ">
+                    The "function" and " var" are known as :
                     </p>
 
                 </div>
-                <div className=" flex  gap-5 flex-col w-1/2">
-      
-      <div class="flex items-center me-4 ">
-        <input checked id="purple-radio" type="radio" value="" name="colored-radio"  />
-        <label for="purple-radio" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Purple</label>
+                <div className=" flex  gap-5 flex-col w-1/2  ml-16 ">      
+      <div class="flex items-center me-4  ">
+        <input checked id="purple-radio" type="radio" value="" name="colored-radio" className=" w-11 h-5"  />
+        <label for="purple-radio" class="ms-2 text-xl font-normal text-gray-900 dark:text-gray-300">Keywords</label>
     </div>
     <div class="flex items-center me-4">
-        <input checked id="purple-radio" type="radio" value="" name="colored-radio"  />
-        <label for="purple-radio" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Purple</label>
+        <input checked id="purple-radio" type="radio" value="" name="colored-radio" className=" w-11 h-5"   />
+        <label for="purple-radio" class="ms-2 text-xl font-normal text-gray-900 dark:text-gray-300">Data types</label>
     </div>
     <div class="flex items-center me-4">
-        <input checked id="purple-radio" type="radio" value="" name="colored-radio"  />
-        <label for="purple-radio" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Purple</label>
+        <input checked id="purple-radio" type="radio" value="" name="colored-radio"  className=" w-11 h-5"  />
+        <label for="purple-radio" class="ms-2 text-xl font-normal text-gray-900 dark:text-gray-300">Declaration statements</label>
     </div>
     <div class="flex items-center me-4">
-        <input checked id="purple-radio" type="radio" value="" name="colored-radio"  />
-        <label for="purple-radio" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Purple</label>
+        <input checked id="purple-radio" type="radio" value="" name="colored-radio" className=" w-11 h-5"  />
+        <label for="purple-radio" class="ms-2 text-xl font-normal text-gray-900 dark:text-gray-300">Prototypes</label>
     </div>
       
 
 
+                    
                     
 
                 </div>
