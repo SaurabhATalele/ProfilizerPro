be88151b35ef2ec974c480689550a12a3b7cb2b6
Saurabh Talele <saurabhatalele@gmail.com>
2024-04-09 13:09:42 +0530

    created logout page

--- client/app/logout/page.js
+++ client/app/logout/page.js
@@ -1,13 +1,33 @@
+"use client";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Lottie from "react-lottie";
+import animationData from "./animations/logoutAnimation";
 
 const page = () => {
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   const router = useRouter();
   useEffect(() => {
     localStorage.removeItem("token");
     router.push("/login");
   }, []);
-  return <div></div>;
+  return (
+    <div className="w-screen flex flex-col justify-center">
+      <div>
+        <Lottie options={defaultOptions} height={400} width={400} />
+      </div>
+      <h1 className="text-center font-bold text-xl text-primary-light dark:text-primary-dark">
+        Logging you out!!! please wait...
+      </h1>
+    </div>
+  );
 };
 
 export default page;
